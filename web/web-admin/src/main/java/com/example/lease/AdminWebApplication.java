ab2f902d594888be3f9529b997ecc213a8d9b099
lamcxnnh <code955@163.com>
2026-06-02 01:10:07 +0800

    mapper配置优化

--- web/web-admin/src/main/java/com/example/lease/AdminWebApplication.java
+++ web/web-admin/src/main/java/com/example/lease/AdminWebApplication.java
@@ -1,11 +1,9 @@
 package com.example.lease;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.example.lease.web.*.mapper")
 
 public class AdminWebApplication {
     public static void main(String[] args) {
